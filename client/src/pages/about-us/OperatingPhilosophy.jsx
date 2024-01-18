7ce35d8cbbbb2fd17d69591e9f2f3b9040b49b7c
anshkakkar15 <kakkaransh40@gmail.com>
2024-01-18 13:22:41 +0530

    diagram responsive

--- client/src/pages/about-us/OperatingPhilosophy.jsx
+++ client/src/pages/about-us/OperatingPhilosophy.jsx
@@ -12,13 +12,6 @@ import {
 } from "./Shapes";
 
 import "styles/About.css";
-// import bgBanner from "assets/page-banners/operating_philosophy_banner.jpg";
-// import shape2 from "assets/shape2.svg";
-// import shape3 from "assets/shape3.svg";
-// import shape4 from "assets/shape4.svg";
-// import shape5 from "assets/shape5.svg";
-// import shape6 from "assets/shape6.svg";
-// import shape7 from "assets/shape7.svg";
 
 import {
   GetOperatingPhilosophyDiagram,
@@ -29,7 +22,7 @@ export default function OperatingPhilosophy() {
   const getOperatingPhilosophy = GetOperatingPhilosophyMutation();
   const diagramData = GetOperatingPhilosophyDiagram();
   const [id, setId] = useState(null);
-  console.log(id);
+  const [color, setColor] = useState("#2A3072");
   return (
     <div className="operating_philosophy_page">
       <Banner
@@ -56,106 +49,140 @@ export default function OperatingPhilosophy() {
         </div>
       </div>
       <div className="container-fluid">
-        {/* <div className="operating_diagram">
+        <div className="operating_diagram">
           <div className="outer_diagram">
-            {diagramData?.data?.data?.map((elm, i) => {
-              return (
-                <React.Fragment key={i}>
-                  <div
-                    onMouseEnter={() => setId(elm?.id)}
-                    onMouseLeave={() => setId(null)}
-                    className={`outer__circle circle_${i + 1}`}
-                  >
-                    <img src={diagramData?.data?.data[0]?.image} alt="icon" />
-                    <img src={shape1} className="img-fluid bm-x" />
-                  </div>
-                  {elm?.id === id ? (
-                    <div className="inner_circle">
-                      <div className="inner_hover_content">
-                        <h3>{elm?.title}</h3>
-                        <p>{elm?.description && elm?.description}</p>
-                      </div>
-                    </div>
-                  ) : null}
-                </React.Fragment>
-              );
-            })}
-            {!id && (
-              <div className="inner_circle">
+            <div
+              onMouseEnter={() => {
+                setId(diagramData?.data?.data[0]?.id);
+                setColor("#34caff");
+              }}
+              onMouseLeave={() => {
+                setColor("#2A3072");
+                setId(null);
+              }}
+              className="outer__circle circle_1"
+            >
+              <Shape1
+                color={diagramData?.data?.data[0]?.id === id && color}
+                image={diagramData?.data?.data[0]?.image}
+              />
+            </div>
+            <div
+              onMouseEnter={() => {
+                setId(diagramData?.data?.data[1]?.id);
+                setColor("#34caff");
+              }}
+              onMouseLeave={() => {
+                setColor("#2A3072");
+                setId(null);
+              }}
+              className="outer__circle circle_2"
+            >
+              <Shape2
+                color={diagramData?.data?.data[1]?.id === id && color}
+                image={diagramData?.data?.data[1]?.image}
+              />
+            </div>
+            <div
+              onMouseEnter={() => {
+                setId(diagramData?.data?.data[2]?.id);
+                setColor("#34caff");
+              }}
+              onMouseLeave={() => {
+                setColor("#2A3072");
+                setId(null);
+              }}
+              className="outer__circle circle_3"
+            >
+              <Shape3
+                color={diagramData?.data?.data[2]?.id === id && color}
+                image={diagramData?.data?.data[2]?.image}
+              />
+            </div>
+            <div
+              onMouseEnter={() => {
+                setId(diagramData?.data?.data[3]?.id);
+                setColor("#34caff");
+              }}
+              onMouseLeave={() => {
+                setColor("#2A3072");
+                setId(null);
+              }}
+              className="outer__circle circle_4"
+            >
+              <Shape4
+                color={diagramData?.data?.data[3]?.id === id && color}
+                image={diagramData?.data?.data[3]?.image}
+              />
+            </div>
+            <div
+              onMouseEnter={() => {
+                setId(diagramData?.data?.data[4]?.id);
+                setColor("#34caff");
+              }}
+              onMouseLeave={() => {
+                setColor("#2A3072");
+                setId(null);
+              }}
+              className="outer__circle circle_5"
+            >
+              <Shape5
+                color={diagramData?.data?.data[4]?.id === id && color}
+                image={diagramData?.data?.data[4]?.image}
+              />
+            </div>
+            <div
+              onMouseEnter={() => {
+                setId(diagramData?.data?.data[5]?.id);
+                setColor("#34caff");
+              }}
+              onMouseLeave={() => {
+                setColor("#2A3072");
+                setId(null);
+              }}
+              className="outer__circle circle_6"
+            >
+              <Shape6
+                color={diagramData?.data?.data[5]?.id === id && color}
+                image={diagramData?.data?.data[5]?.image}
+              />
+            </div>
+            <div
+              onMouseEnter={() => {
+                setId(diagramData?.data?.data[6]?.id);
+                setColor("#34caff");
+              }}
+              onMouseLeave={() => {
+                setColor("#2A3072");
+                setId(null);
+              }}
+              className="outer__circle circle_7"
+            >
+              <Shape7
+                color={diagramData?.data?.data[6]?.id === id && color}
+                image={diagramData?.data?.data[6]?.image}
+              />
+            </div>
+            <div className="inner_circle">
+              {diagramData?.data?.data?.map((elm, i) => {
+                return (
+                  <React.Fragment key={i}>
+                    {elm?.id === id ? (
+                      <p>{elm?.description && elm?.description}</p>
+                    ) : null}
+                  </React.Fragment>
+                );
+              })}
+              {!id && (
                 <p>
                   {getOperatingPhilosophy?.data?.data?.diagramDescription &&
                     getOperatingPhilosophy?.data?.data?.diagramDescription}
                 </p>
-              </div>
-            )}
-          </div>
-        </div> */}
-        <div className="operating_diagram">
-          <div className="outer_diagram">
-            <div className="outer__circle circle_1">
-              <Shape1 />
-            </div>
-            <div className="outer__circle circle_2">
-              <Shape2 />
-            </div>
-            <div className="outer__circle circle_3">
-              <Shape3 />
-            </div>
-            <div className="outer__circle circle_4">
-              <Shape4 />
-            </div>
-            <div className="outer__circle circle_5">
-              <Shape5 />
-            </div>
-            <div className="outer__circle circle_6">
-              <Shape6 />
-            </div>
-            <div className="outer__circle circle_7">
-              <Shape7 />
-            </div>
-            <div className="inner_circle">
-              <p>
-                {getOperatingPhilosophy?.data?.data?.diagramDescription &&
-                  getOperatingPhilosophy?.data?.data?.diagramDescription}
-              </p>
+              )}
             </div>
           </div>
         </div>
       </div>
     </div>
   );
-}
-
-{
-  /* <div className="operating_diagram">
-<div className="outer_diagram">
-  <div className="outer__circle circle_1">
-    <img src={require("assets/operation_icon_1.png")} alt="icon" />
-  </div>
-  <div className="outer__circle circle_2">
-    <img src={require("assets/operation_icon_2.png")} alt="icon" />
-  </div>
-  <div className="outer__circle circle_3">
-    <img src={require("assets/operation_icon_3.png")} alt="icon" />
-  </div>
-  <div className="outer__circle circle_4">
-    <img src={require("assets/operation_icon_4.png")} alt="icon" />
-  </div>
-  <div className="outer__circle circle_5">
-    <img src={require("assets/operation_icon_5.png")} alt="icon" />
-  </div>
-  <div className="outer__circle circle_6">
-    <img src={require("assets/operation_icon_6.png")} alt="icon" />
-  </div>
-  <div className="outer__circle circle_7">
-    <img src={require("assets/operation_icon_7.png")} alt="icon" />
-  </div>
-  <div className="inner_circle">
-    <p>
-      {getOperatingPhilosophy?.data?.data?.diagramDescription &&
-        getOperatingPhilosophy?.data?.data?.diagramDescription}
-    </p>
-  </div>
-</div>
-</div> */
 }
