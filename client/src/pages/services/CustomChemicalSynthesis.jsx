ea4b5aaf973482b2a5d06af7582ca1ff7aa5625e
anshkakkar15 <kakkaransh40@gmail.com>
2024-02-09 16:39:12 +0530

    changes done

--- client/src/pages/services/CustomChemicalSynthesis.jsx
+++ client/src/pages/services/CustomChemicalSynthesis.jsx
@@ -61,6 +61,29 @@ export default function CustomChemicalSynthesis() {
   return (
     <div className="custom_chemical_synthesis_page">
       <div className="container-fluid">
+        <div
+          className="custom_page_banner"
+          style={{
+            backgroundImage: `linear-gradient(rgba(42, 48, 114, 0.85), rgba(42, 48, 114, 0.85)),url(${banner})`,
+          }}
+        >
+          <h2 className="main_top_heading text-white">
+            {getExpertiesIncludes?.data?.data?.heading &&
+              getExpertiesIncludes?.data?.data?.heading}
+          </h2>
+          <div className="lists_chemical">
+            {getExpertiesIncludes?.data?.data?.expertiseList &&
+              getExpertiesIncludes?.data?.data?.expertiseList
+                ?.split("@@")
+                ?.map((elm, i) => {
+                  return (
+                    <div key={i} className="chem_1">
+                      <span>{elm}</span>
+                    </div>
+                  );
+                })}
+          </div>
+        </div>
         <div className="chemical_top_sec">
           <h1 className="main_top_heading text-center">
             Custom Chemical Synthesis
@@ -102,29 +125,6 @@ export default function CustomChemicalSynthesis() {
               />
             </div>
           )}
-        </div>
-        <div
-          className="custom_page_banner"
-          style={{
-            backgroundImage: `linear-gradient(rgba(42, 48, 114, 0.85), rgba(42, 48, 114, 0.85)),url(${banner})`,
-          }}
-        >
-          <h2 className="main_top_heading text-white">
-            {getExpertiesIncludes?.data?.data?.heading &&
-              getExpertiesIncludes?.data?.data?.heading}
-          </h2>
-          <div className="lists_chemical">
-            {getExpertiesIncludes?.data?.data?.expertiseList &&
-              getExpertiesIncludes?.data?.data?.expertiseList
-                ?.split("@@")
-                ?.map((elm, i) => {
-                  return (
-                    <div key={i} className="chem_1">
-                      <span>{elm}</span>
-                    </div>
-                  );
-                })}
-          </div>
         </div>
       </div>
     </div>
