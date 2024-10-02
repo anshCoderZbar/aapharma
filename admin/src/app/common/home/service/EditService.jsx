3bd65658402ab6179404c5c9c4af5d892ff32dd6
Anshkakkar15 <kakkaransh40@gmail.com>
2024-10-02 21:27:03 +0530

    changes in small molecule drug page and lead developement page

--- admin/src/app/common/home/service/EditService.jsx
+++ admin/src/app/common/home/service/EditService.jsx
@@ -23,7 +23,7 @@ export default function EditService() {
   useEffect(() => {
     const defaultValues = {};
     defaultValues.heading = getSingleService?.data?.data[0]?.heading;
-    defaultValues.heading = getSingleService?.data?.data[0]?.url;
+    defaultValues.url = getSingleService?.data?.data[0]?.url;
     defaultValues.icon = getSingleService?.data?.data[0]?.icon;
     defaultValues.featuredImage =
       getSingleService?.data?.data[0]?.featuredImage;
