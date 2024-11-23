5d4792be9eef209ebcc159191e237652c7211a2a
Anshkakkar15 <kakkaransh40@gmail.com>
2024-11-24 01:17:43 +0530

    accrediation page design done

--- client/src/pages/capabilities/Accreditations.jsx
+++ client/src/pages/capabilities/Accreditations.jsx
@@ -14,35 +14,35 @@ export default function Accreditations() {
         extra="white_head process_top_banner"
       />
       <div className="container-fluid">
-        <div className="row accreditations_content">
-          <div className="col-md-6">
-            <div className="accreditations_img">
-              <img
-                src={
-                  getAccreditation?.data?.data?.image &&
-                  getAccreditation?.data?.data?.image
-                }
-                alt="accreditations"
-              />
+        <div className="max-container">
+          <div className="row accreditations_content">
+            <div className="col-md-6">
+              <div className="accreditations_img">
+                <img
+                  src={
+                    getAccreditation?.data?.data?.image &&
+                    getAccreditation?.data?.data?.image
+                  }
+                  alt="accreditations"
+                />
+              </div>
             </div>
-          </div>
-          <div className="col-md-6">
-            <div className="accreditations_main_content">
-              {/* <h1 className="main_top_heading">
+            <div className="col-md-6">
+              <div className="accreditations_main_content">
+                {/* <h1 className="main_top_heading">
                 {getAccreditation?.data?.data?.heading &&
                   getAccreditation?.data?.data?.heading}
               </h1> */}
-              <div
-                dangerouslySetInnerHTML={{
-                  __html:
-                    getAccreditation?.data?.data?.description &&
-                    getAccreditation?.data?.data?.description,
-                }}
-              />
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html:
+                      getAccreditation?.data?.data?.description &&
+                      getAccreditation?.data?.data?.description,
+                  }}
+                />
+              </div>
             </div>
           </div>
-        </div>
-        <div className="max-container">
           <div className="row mb-5">
             <div className="col-12 col-sm-6 col-md-4">
               <div className="accrediation_btm_card">
