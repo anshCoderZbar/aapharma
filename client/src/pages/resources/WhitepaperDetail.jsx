a6229592dbd782ce1c8542e4ea473b67fdaaf9ad
NiteshLangyan <46992544+NiteshLangyan@users.noreply.github.com>
2025-06-11 08:36:37 +0530

    Update WhitepaperDetail.jsx breadcrumb changes White Papers

--- client/src/pages/resources/WhitepaperDetail.jsx
+++ client/src/pages/resources/WhitepaperDetail.jsx
@@ -20,7 +20,7 @@ export default function WhitepaperDetail() {
           singleWhitepaper?.data?.data?.heading
         }
         background={`linear-gradient(90deg, rgba(48, 48, 114, 1) 100%, rgba(48, 48, 114, 0) 100%)`}
-        breadCrum={"whitepaper"}
+        breadCrum={"White Paper"}
         extra="white_head white_paper_single"
       />
       <div className="container-fluid">
